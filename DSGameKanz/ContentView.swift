3ee678bcf99348737e273dcaefc6fd38172b071b
Maryam <maryamjalzh@gmail.com>
2025-11-20 15:04:35 +0300

    Initial Commit

---
+++ DSGameKanz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DSGameKanz
+//
+//  Created by Maryam Jalal Alzahrani on 29/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DSGameKanz/DSGameKanzApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSGameKanzApp.swift
+//  DSGameKanz
+//
+//  Created by Maryam Jalal Alzahrani on 29/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct DSGameKanzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
