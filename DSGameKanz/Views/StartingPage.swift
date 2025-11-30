171adb12a875e2c47c4becae346483aff71e00e4
engsaramo <446000253@pnu.edu.sa>
2025-11-30 15:22:12 +0300

    added  startibg page

---
+++ DSGameKanz/Views/StartingPage.swift
@@ -0,0 +1,18 @@
+//
+//  StartingPage.swift
+//  DSGameKanz
+//
+//  Created by Sarah Aleissa on 30/11/2025.
+//
+
+import SwiftUI
+
+struct StartingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartingPage()
+}
