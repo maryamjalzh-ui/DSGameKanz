c365b15cd2363e8de5ba0fba970e48f73f45bad1
engsaramo <446000253@pnu.edu.sa>
2025-12-02 14:21:41 +0300

    .

---
+++ DSGameKanz/Views/CharacterChoice.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterChoice.swift
+//  DSGameKanz
+//
+//  Created by Sarah Aleissa on 01/12/2025.
+//
+
+import SwiftUI
+
+struct CharacterChoice: View {
+    var body: some View {
+//        .navigationBarBackButtonHidden(true)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+         
+    }
+}
+
+#Preview {
+    CharacterChoice()
+}
