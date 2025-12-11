bcdf86ca44df68086ba434c6523b4015c70a3e87
engsaramo <446000253@pnu.edu.sa>
2025-12-11 16:13:19 +0300

    ..s

---
+++ DSGameKanz/Views/LanguageManager.swift
@@ -0,0 +1,18 @@
+//
+//  LanguageManager.swift
+//  DSGameKanz
+//
+//  Created by Sarah Aleissa on 11/12/2025.
+//
+
+import SwiftUI
+
+struct LanguageManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LanguageManager()
+}
